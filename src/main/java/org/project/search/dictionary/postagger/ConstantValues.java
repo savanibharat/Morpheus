3fba5213b0d626afb953f06d73d965678197acb5
BHARAT SAVANI <savanibharat@gmail.com>
2015-05-03 13:55:27 -0700

    ConstantValue final class for String matching

---
+++ src/main/java/org/project/search/dictionary/postagger/ConstantValues.java
@@ -0,0 +1,12 @@
+package org.project.search.dictionary.postagger;
+
+final public class ConstantValues {
+
+	public final static String NOUN = "NOUN";
+	public final static String ADVERB = "ADVERB";
+	public final static String ADJECTIVE = "ADJECTIVE";
+	public final static String VERB = "VERB";
+	public final static String ADVERB_SHORT = "ADV";
+	public final static String ADJECTIVE_SHORT = "ADJ";
+
+}
